e39db5ef41eb4aab189ebc2f4111595efba7669e
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-19 22:33:55 +0600

    Implement book module with interface, model, and route; set up routes in app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import routes from "./routes";
 
 export const app = express();
 
 //middleware
 app.use([cors(), express.json()]);
+
+//routes
+app.use(routes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
