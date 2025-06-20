3080c09d54e664b608f54b821d14e2ed2c573af6
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-20 15:02:31 +0600

    Implement createBorrowBook function with validation and error handling

--- src/modules/borrow/borrow.controller.ts
+++ src/modules/borrow/borrow.controller.ts
@@ -1,6 +1,55 @@
 import { Request, Response } from "express";
+import Book from "../book/book.model";
 
-const createBorrowBook = async (req: Request, res: Response) => {};
+const createBorrowBook = async (req: Request, res: Response) => {
+  try {
+    const { book, quantity } = req.body;
+
+    const findBook = await Book.findById(book);
+
+    //check the book is available
+    if (!findBook) {
+      return res.status(404).json({
+        message: "Book not found",
+        success: false,
+        error: "Invalid book ID",
+      });
+    }
+
+    //Verify the book has enough available copies.
+    if (!((findBook?.copies as number) >= quantity)) {
+      return res.status(400).json({
+        message: "Book does not have enough available copies",
+        success: false,
+        error: "Not enough copies available",
+      });
+    }
+
+    // Deduct the requested quantity from the book's available copies
+    await Book.findByIdAndUpdate(
+      book,
+      {
+        $inc: {
+          copies: -quantity,
+        },
+      },
+      { new: true }
+    );
+
+    console.log(findBook);
+
+    res.status(201).json({
+      success: true,
+      message: "Book borrowed successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Book borrowed failed",
+      success: false,
+      error,
+    });
+  }
+};
 
 export const borrowBookController = {
   createBorrowBook,
